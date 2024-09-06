9c69bedf29ce4b4c3d5f282c1c184c393a898d2d
breadbread1984 <breadbread1984@163.com>
2024-09-06 14:00:36 +0800

    update

---
+++ main/main_decision_kan.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import numpy as np
+import torch
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from run.run_decision_kan import run_dk
+
+torch.manual_seed(1)
+np.random.seed(1)
+
+if __name__ == "__main__":
+    run_dk()
